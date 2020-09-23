21f6174129f92a5f1a9693d34da1411f54d78cd9
MaksymDanylenko <maksimdanilenko1999@gmail.com>
2020-09-23 22:37:56 +0300

    Events solution

---
+++ EventsPractice/PiggyBank.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventsPractice
+{
+    public delegate void TakeDelegate(int value);
+
+    class PiggyBank
+    {
+        public event TakeDelegate takeEvent;
+
+        private int _money;
+
+        public int Money
+        {
+            get { return _money; }
+            set
+            {
+                _money = value;
+                takeEvent(_money);
+            }
+        }
+
+
+    }
+}
---
+++ EventsPractice/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace EventsPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = "";
+            PiggyBank piggyBank = new PiggyBank();
+
+            piggyBank.takeEvent += MoneyChanged;
+            piggyBank.takeEvent += Reached;
+
+            do
+            {
+                Console.WriteLine("How much to deposit?");
+                command = Console.ReadLine();
+                if (command.Equals("exit"))
+                {
+                    return;
+                }
+
+                int value = int.Parse(command);
+                piggyBank.Money += value;
+
+            } while (!command.Equals("exit"));
+        }
+
+        static void MoneyChanged(int value)
+        {
+            Console.WriteLine($"The money amount is {value}");
+        }
+
+        static void Reached(int value)
+        {
+            if (value >= 500)
+            {
+                Console.WriteLine($"You reached your savings goal! You have {value}");
+            }
+        }
+    }
+}
